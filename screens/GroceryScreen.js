0137d04c32157a4c9866e6c367f805ddecdff81e
Diego RC <diego.rcsd@gmail.com>
2022-12-01 14:32:16 -0700

    loops fixed

--- screens/GroceryScreen.js
+++ screens/GroceryScreen.js
@@ -1,5 +1,5 @@
-import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import React, { useState, useEffect } from 'react';
 import {
   Button,
   Appbar,
@@ -7,18 +7,101 @@ import {
   TextInput,
   Divider,
 } from 'react-native-paper';
+import { auth, db } from '../firebase';
+import {
+  doc,
+  setDoc,
+  getDoc,
+  addDoc,
+  collection,
+  onSnapshot,
+  getDocs,
+} from 'firebase/firestore';
 
 const GroceryScreen = () => {
   const [checked, setChecked] = React.useState(false);
   const [grocery, setGrocery] = React.useState('');
+  const [userDetails, setUserDetails] = React.useState([]);
+  const [items, setItems] = React.useState([]);
 
-  const items = ['bananas', 'milk', 'eggs', 'steak', 'oreos'];
+  useEffect(() => {
+    (async () => {
+      const docRef = doc(db, 'users', auth.currentUser.uid);
+      const docSnap = await getDoc(docRef);
+      setUserDetails(docSnap.data());
+    })();
+  }, []);
+
+  // const readDB = () => {
+  // console.log('click');
+  let collectionRef = collection(
+    db,
+    'Families',
+    `${userDetails.familyId}`,
+    'Grocery List'
+  );
+
+  onSnapshot(collectionRef, (querySnapshot) => {
+    let newItems = [];
+    querySnapshot.docs.forEach((doc) => {
+      newItems.push(doc.data().item);
+    });
+    console.log('please no loop');
+    helper(newItems);
+  });
+
+  /** THINGS TO TRY
+   
+      do a length check for both arrays
+      sate v. db
+      one time read option (some fb method)
+
+  */
+
+  const helper = (newItems) => {
+    console.log('please no loop v2');
+    if (newItems.length !== items.length) {
+      setItems(newItems);
+    }
+  };
+
+  // console.log();
+
+  // };
+  // console.log('testa', readDB());
+
+  // useEffect(() => {
+  //   let collectionRef = collection(
+  //     db,
+  //     'Families',
+  //     `${userDetails.familyId}`,
+  //     'Grocery List'
+  //   );
+
+  //   onSnapshot(collectionRef, (querySnapshot) => {
+  //     querySnapshot.forEach((doc) => {
+  //       // test2.push(doc.data().item);
+  //       console.log(doc.data().item);
+  //     });
+  //   });
+
+  // }, [items]);
 
   const handleSubmit = () => {
-    items.forEach((item) => {
-      const newItem = doc(db, 'grocerylist', auth.currentUser.uid);
-      setDoc(newItem, { item: item }, { merge: true });
-    });
+    addDoc(
+      collection(
+        doc(collection(db, 'Families'), userDetails.familyId),
+        'Grocery List'
+      ),
+      {
+        name: userDetails.name,
+        userId: auth.currentUser.uid,
+        familyId: userDetails.familyId,
+        item: grocery,
+      }
+    );
+    // if we do unsub here we have to resub
+    // console.log('readDB func', readDB());
   };
 
   return (
@@ -38,7 +121,6 @@ const GroceryScreen = () => {
                 onPress={() => {
                   setChecked(!checked);
                 }}
-                key={item}
               />
               <Divider />
             </>
@@ -52,8 +134,8 @@ const GroceryScreen = () => {
           onChangeText={(grocery) => setGrocery(grocery)}
           onSubmitEditing={handleSubmit}
         />
-        {/* <TouchableHighlight onPress={handleSubmit}>
-          <Text>Press this button to submit editing</Text>
+        {/* <TouchableHighlight onPress={readDB}>
+          <Text>TEST BUTTON</Text>
         </TouchableHighlight> */}
       </View>
     </>
