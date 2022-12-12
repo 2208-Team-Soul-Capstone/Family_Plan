6807a2cff079a281eee506e9ed1f1a406f4f5484
davis86t <4833583@gmail.com>
2022-12-11 20:08:46 -0600

    Calendar now reading from and writing to DB

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,52 +1,104 @@
 import { StyleSheet, View, TouchableOpacity, Modal, TextInput, Keyboard } from 'react-native'
-import React, { useState } from 'react'
-import { Button, Appbar, List, Text } from 'react-native-paper';
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
-import { Divider, Snackbar } from 'react-native-paper';
-import { Card, Avatar } from 'react-native-paper';
+import React, { useState, useEffect, useCallback } from 'react'
+import { Button, Text } from 'react-native-paper';
+import {  Agenda } from 'react-native-calendars';
+import { Card } from 'react-native-paper';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import { auth, db } from '../firebase';
+import { onSnapshot, doc, addDoc, collection, query, getDoc } from 'firebase/firestore';
+import { useFocusEffect } from '@react-navigation/native';
 
-
-const timeToString = (time) => {
-  const date = new Date(time);
-  return date.toISOString().split('T')[0];
-};
 const CalendarScreen = () => {
   const [items, setItems] = useState({});
   const [modalVisible, setModalVisible] = useState(false)
-  const [newTaskName, setNewTaskName] = useState(null)
-  const [newTaskDescription, setNewTaskDescription] = useState(null)
-  const [newTaskPoints, setNewTaskPoints] = useState(null)
-  const [addTaskSnack, setAddTaskSnack] = useState(false)
-  // const [mode, setMode] = useState('date');
+  const [eventTitle, setEventTitle] = useState(null)
+  const [userDetails, setUserDetails] = useState([])
+  const [time, setTime] = useState(new Date(Date.now()));
+  const [date, setDate] = useState(new Date());
+  const [startTime, setStartTime] = useState(new Date(Date.now()));
+  const [loading, setLoading] = useState(false)
 
-  const onDismissAppTaskSnackbar = () => setAddTaskSnack(false);
-  const date = new Date()
-
-  const loadItems = (day) => {
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = timeToString(time);
-        if (!items[strTime]) {
-          items[strTime] = [];
-          const numItems = Math.floor(Math.random() * 3 + 1);
-          // for (let j = 0; j < numItems; j++) {
-          //   items[strTime].push({
-          //     name: 'Item for ' + strTime + ' #' + j,
-          //     height: Math.max(50, Math.floor(Math.random() * 150)),
-          //   });
-          // }
+  //Get User data from DB
+  useEffect(() => {
+    try {
+      const getData = async () => {
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          setUserDetails(docSnap.data());
+          setUserDetails(() => ({ ...docSnap.data() }));
         }
       }
+      getData();
+    } catch (err) {
+      console.log(err)
+    }
+  }, [loading]);
 
-      const newItems = {};
-      Object.keys(items).forEach((key) => {
-        newItems[key] = items[key];
+  const calendarRef = collection(
+    db,
+    'Families',
+    `${userDetails.familyId}`,
+    'Calendar'
+  );
+
+  //Get Agenda data from DB
+  useFocusEffect(
+    useCallback(() => {
+      let newItems = {}
+      const q = query(calendarRef)
+      const unsubscribe = onSnapshot(q, (snapshot) => {
+        snapshot.docs.forEach((doc) => {
+          Object.assign(newItems, doc.data())
+        })
+        setItems(newItems)
       });
-      setItems(newItems);
+      return () => unsubscribe();
+    }, [userDetails])
+  )
+
+  // useEffect(() => {
+  //   if (items === {}) {
+  //     loadItems()
+  //   }
+  // }, [items])
+
+  const loadItems = () => {
+    setTimeout(() => {
+      console.log('Items Loaded!')
     }, 1000);
   };
+
+  const toggleModalView = () => {
+    setModalVisible(!modalVisible);
+  };
+
+
+  // Add Event to DB
+  const handleAddEvent = () => {
+    addDoc(collection(doc(collection(db, 'Families'), `${userDetails.familyId}`),
+      'Calendar'
+    ),
+      {
+        [`${date.toISOString().split('T')[0]}`]:
+          [{ 'startTime': [`${startTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`], 'name': [`${eventTitle}`] }]
+      })
+      setLoading(!loading)
+  };
+
+  // function reloadAgenda() {
+  //   setItems({})
+  // }
+
+  function onDateSelected(event, value) {
+    setDate(value)
+  };
+
+  function onStartTimeSelected(event, value) {
+    setStartTime(value);
+  };
+
+  // Render each Event to Agenda view
   const renderItem = (item) => {
     return (
       <TouchableOpacity style={{ marginRight: 10, marginTop: 17 }}>
@@ -58,7 +110,9 @@ const CalendarScreen = () => {
                 justifyContent: 'space-between',
                 alignItems: 'center',
               }}>
-              {/* <Avatar.Text label="M" /> */}
+              <Text>
+                {`${item.startTime} - ${item.name}`}
+              </Text>
             </View>
           </Card.Content>
         </Card>
@@ -66,8 +120,7 @@ const CalendarScreen = () => {
     );
   };
 
-
-
+  // Modal pop up for adding events
   return (
     <View style={{ flex: 1 }}>
       <Modal
@@ -84,18 +137,35 @@ const CalendarScreen = () => {
           <View style={styles.modalView}>
             <View style={{ flexDirection: 'row', width: '90%', marginTop: 10, marginBottom: 20 }}>
               <View style={styles.leftContainer}>
-                <Text variant="bodyLarge">Cancel</Text>
+                <Text
+                  style={{ color: 'red' }}
+                  onPress={toggleModalView}
+                  variant="bodyLarge"
+                >
+                  Cancel
+                </Text>
               </View>
-              <Text style={{ alignSelf: 'center' }} variant="bodyLarge">New Event</Text>
+              <Text
+                style={{ alignSelf: 'center' }}
+                variant="bodyLarge"
+              >
+                New Event
+              </Text>
               <View style={styles.rightContainer}>
-                <Text variant="bodyLarge">Add</Text>
+                <Text
+                  style={{ color: 'red' }}
+                  onPress={() => { toggleModalView(), handleAddEvent(), loadItems() }}
+                  variant="bodyLarge"
+                >
+                  Add
+                </Text>
               </View>
             </View>
             <View style={{ width: '95%' }}>
               <TextInput
                 placeholder={'Title'}
-                value={newTaskDescription}
-                onChangeText={(text) => setNewTaskDescription(text)}
+                value={eventTitle}
+                onChangeText={(text) => { setEventTitle(text), console.log(eventTitle) }}
                 style={styles.textInput}
                 onSubmitEditing={Keyboard.dismiss}
                 placeholderTextColor="gray"
@@ -113,64 +183,19 @@ const CalendarScreen = () => {
                   mode="date"
                   is24Hour={true}
                   display='calendar'
-                // onChange={console.log('onChange from date picker fired')}
+                  onChange={onDateSelected}
                 />
                 <DateTimePicker
                   style={{ marginLeft: 5, marginBottom: 5 }}
                   testID='dateTimePicker'
-                  value={date}
+                  value={time}
                   mode="time"
                   is24Hour={true}
                   display='clock'
-                // onChange={console.log('onChange from date picker fired')}
+                  onChange={onStartTimeSelected}
                 />
               </View>
             </View>
-            <View style={{ flexDirection: 'row', width: '90%' }}>
-              <View style={styles.leftContainer}>
-                <Text style={{ textAlign: 'left' }} variant="bodyLarge">Ends</Text>
-              </View>
-              <View style={styles.rightContainer}>
-                <DateTimePicker
-                  style={{ marginRight: 5, marginTop: 5 }}
-                  testID='dateTimePicker'
-                  value={date}
-                  mode="date"
-                  is24Hour={true}
-                  display='calendar'
-                // onChange={console.log('onChange from date picker fired')}
-                />
-                <DateTimePicker
-                  style={{ marginLeft: 5, marginTop: 5 }}
-                  testID='dateTimePicker'
-                  value={date}
-                  mode="time"
-                  is24Hour={true}
-                  display='clock'
-                // onChange={console.log('onChange from date picker fired')}
-                />
-              </View>
-            </View>
-            {/* <Button
-              mode="contained" style={styles.addTaskButton}
-              onPress={() => addTask()}
-            >
-              <Text style={styles.buttonText}>Submit</Text>
-            </Button>
-            <Button
-              mode="contained" style={styles.addTaskButton}
-              onPress={() => setModalVisible(false)}
-            >
-              <Text style={styles.buttonText}>Cancel</Text>
-            </Button> */}
-          </View>
-          <View style={styles.snackbarsAddTask}>
-            <Snackbar
-              visible={addTaskSnack}
-              onDismiss={onDismissAppTaskSnackbar}
-            >
-              Please fill out all fields.
-            </Snackbar>
           </View>
         </View>
       </Modal>
@@ -180,11 +205,17 @@ const CalendarScreen = () => {
         selected={new Date()}
         renderItem={renderItem}
       />
-      <Button icon="calendar-plus" onPress={() => { setModalVisible(true), console.log(items) }} mode="contained" style={styles.addEventButton}>Add an Event</Button>
+      <Button
+        icon="calendar-plus"
+        onPress={() => { setModalVisible(true) }}
+        mode="contained"
+        style={styles.addEventButton}
+      >
+        Add an Event
+      </Button>
     </View>
   );
 };
-
 
 export default CalendarScreen
 
@@ -224,7 +255,7 @@ const styles = StyleSheet.create({
   },
   addEventButton: {
     width: '100%',
-    alignSelf: 'center', 
+    alignSelf: 'center',
   },
   textInput: {
     width: '95%',
